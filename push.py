da31ff623bdab6a43cb47e5dbf7f7e5e77d0e8b6
Yaolin Ge <geyaolin@gmail.com>
2022-06-18 18:49:28 +0200

    Upload data

--- push.py
+++ push.py
@@ -1,6 +1,10 @@
 import os
 import time
 
-for i in range(5):
+for i in range(2):
+
+    os.system("git add .")
+    os.system("git commit -m \"Upload data\"")
+    os.system("git push --all")
     time.sleep(10)
-    os.system("sh bot.sh")
+
