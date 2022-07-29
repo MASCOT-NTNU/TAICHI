013d1a14516534d859baf9914ae0aa3f8cb64de0
berild <moutzebe@gmail.com>
2022-07-29 18:34:29 +0200

    add new simplified setup

--- Nidelva3D/src/SPDE/spde.py
+++ Nidelva3D/src/SPDE/spde.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy import sparse
 from sksparse.cholmod import cholesky
 from TAICHI.Nidelva3D.Config.Config import FILEPATH
-DEFAULT_NUM_SAMPLES = 250 # 150 is too much
+DEFAULT_NUM_SAMPLES = 250 
 
 class spde:
     def __init__(self, model = 2, reduce = False, method = 1, prev = False):
@@ -14,77 +14,29 @@ class spde:
             method (int, optional): If model should contain fixed effects on the SINMOD mean. Defaults to 1.
             prev (bool, optional): Loading previous model (used to clear memory)
         """
-        self.M = 45
-        self.N = 45
-        self.P = 11
+        self.M = 50
+        self.N = 55
+        self.P = 6
         if prev:
             self.loadModel()
         else:
-            self.model = model
-            self.method = method
             self.reduced = reduce
             if self.reduced:
                 self.P = 7
             self.n = self.M*self.N*self.P
-                
         
-        if self.method == 1:
-            if self.reduced:
-                self.mu3 = np.load(FILEPATH + 'models/prior_small.npy')
-                tmp = np.load(FILEPATH + 'models/S1r.npy')
-                self.Stot = sparse.csc_matrix((np.array(tmp[:,2],dtype = "float32"), (tmp[:,0].astype('int32'), tmp[:,1].astype('int32'))), shape=(self.n,self.n)) 
-            else: 
-                self.mu3 = np.load(FILEPATH + 'models/prior.npy')
-                tmp = np.load(FILEPATH + 'models/S1.npy')
-                self.Stot = sparse.csc_matrix((np.array(tmp[:,2],dtype = "float32"), (tmp[:,0].astype('int32'), tmp[:,1].astype('int32'))), shape=(self.n,self.n)) 
-            if not prev:
-                self.mu = self.mu3
-        else:
-            if self.reduced:
-                self.mu3 = np.load(FILEPATH + 'models/prior_small.npy')
-                tmp = np.load(FILEPATH + 'models/S2r.npy')
-                self.Stot = sparse.csc_matrix((np.array(tmp[:,2],dtype = "float32"), (tmp[:,0].astype('int32'), tmp[:,1].astype('int32'))), shape=(self.n,self.n+2)) 
-            else: 
-                self.mu3 = np.load(FILEPATH + 'models/prior.npy')
-                tmp = np.load(FILEPATH + 'models/S2.npy')
-                self.Stot = sparse.csc_matrix((np.array(tmp[:,2],dtype = "float32"), (tmp[:,0].astype('int32'), tmp[:,1].astype('int32'))), shape=(self.n,self.n+2)) 
-            if not prev:
-                self.mu = self.mu3
-                self.mu2 = np.hstack([np.zeros(self.n),0,1]).reshape(-1,1) # Mean of random effect and betas
-                self.beta0 = 0
-                self.beta1 = 1
-                
+        self.mu3 = np.load(FILEPATH + 'models/prior.npy')
+        tmp = np.load(FILEPATH + 'models/S.npy')
+        self.Stot = sparse.csc_matrix((np.array(tmp[:,2],dtype = "float32"), (tmp[:,0].astype('int32'), tmp[:,1].astype('int32'))), shape=(self.n,self.n+2)) 
         if not prev:
-            if self.method == 1:
-                if self.model == 1:
-                    if self.reduced:
-                        tmp = np.load(FILEPATH + 'models/SA1r.npy') 
-                        self.Q = sparse.csc_matrix((np.array(tmp[:,2],dtype = "float32"), (tmp[:,0].astype('int32'), tmp[:,1].astype('int32'))), shape=(self.n,self.n))
-                    else:
-                        tmp = np.load(FILEPATH + 'models/SA1.npy') 
-                        self.Q = sparse.csc_matrix((np.array(tmp[:,2],dtype = "float32"), (tmp[:,0].astype('int32'), tmp[:,1].astype('int32'))), shape=(self.n,self.n)) 
-                else:
-                    if self.reduced:
-                        tmp = np.load(FILEPATH + 'models/NA1r.npy')
-                        self.Q = sparse.csc_matrix((np.array(tmp[:,2],dtype = "float32"), (tmp[:,0].astype('int32'), tmp[:,1].astype('int32'))), shape=(self.n,self.n))
-                    else:
-                        tmp = np.load(FILEPATH + 'models/NA1.npy')
-                        self.Q = sparse.csc_matrix((np.array(tmp[:,2],dtype = "float32"), (tmp[:,0].astype('int32'), tmp[:,1].astype('int32'))), shape=(self.n,self.n)) 
-            else:
-                if self.model == 1:
-                    if self.reduced:
-                        tmp = np.load(FILEPATH + 'models/SA2r.npy')
-                        self.Q = sparse.csc_matrix((np.array(tmp[:,2],dtype = "float32"), (tmp[:,0].astype('int32'), tmp[:,1].astype('int32'))), shape=(self.n+2,self.n+2))
-                    else:
-                        tmp = np.load(FILEPATH + 'models/SA2.npy')
-                        self.Q = sparse.csc_matrix((np.array(tmp[:,2],dtype = "float32"), (tmp[:,0].astype('int32'), tmp[:,1].astype('int32'))), shape=(self.n+2,self.n+2)) 
-                else:
-                    if self.reduced:
-                        tmp = np.load(FILEPATH + 'models/NA2r.npy')
-                        self.Q = sparse.csc_matrix((np.array(tmp[:,2],dtype = "float32"), (tmp[:,0].astype('int32'), tmp[:,1].astype('int32'))), shape=(self.n+2,self.n+2))
-                    else:
-                        tmp = np.load(FILEPATH + 'models/NA2.npy')
-                        self.Q = sparse.csc_matrix((np.array(tmp[:,2],dtype = "float32"), (tmp[:,0].astype('int32'), tmp[:,1].astype('int32'))), shape=(self.n+2,self.n+2)) 
+            self.mu = self.mu3
+            self.mu2 = np.hstack([np.zeros(self.n),0,1]).reshape(-1,1) # Mean of random effect and betas
+            self.beta0 = 0
+            self.beta1 = 1
+            
+        if not prev:
+            tmp = np.load(FILEPATH + 'models/NA.npy')
+            self.Q = sparse.csc_matrix((np.array(tmp[:,2],dtype = "float32"), (tmp[:,0].astype('int32'), tmp[:,1].astype('int32'))), shape=(self.n+2,self.n+2)) 
         
         self.Q_fac = cholesky(self.Q)
         self.sigma = np.load(FILEPATH + 'models/sigma.npy') # measurement noise robot [0] and SINMOD fitted noise [1]
@@ -102,13 +54,9 @@ class spde:
         Args:
             n (int, optional): Number of realizations. Defaults to 1.
         """
-        if self.method == 2:
-            z = np.random.normal(size = (self.n+2)*n).reshape((self.n+2),n)
-            data = self.Q_fac.apply_Pt(self.Q_fac.solve_Lt(z,use_LDLt_decomposition=False)) 
-            data = data[:self.n,:] + self.mu3.reshape(-1,1) + np.random.normal(size = self.n*n).reshape(self.n,n)*self.sigma[1]
-        else:
-            z = np.random.normal(size = self.n*n).reshape(self.n,n)
-            data = self.Q_fac.apply_Pt(self.Q_fac.solve_Lt(z,use_LDLt_decomposition=False)) + np.random.normal(size = self.n)*self.sigma[1] + self.mu3
+        z = np.random.normal(size = (self.n+2)*n).reshape((self.n+2),n)
+        data = self.Q_fac.apply_Pt(self.Q_fac.solve_Lt(z,use_LDLt_decomposition=False)) 
+        data = data[:self.n,:] + self.mu3.reshape(-1,1) + np.random.normal(size = self.n*n).reshape(self.n,n)*self.sigma[1]
         return(data)
 
     def cholesky(self,Q):
@@ -130,8 +78,7 @@ class spde:
         """
         np.save(FILEPATH + 'models/mucond.npy', self.mu)
         np.save(FILEPATH + 'models/smvarcond.npy', self.mvar())
-        if self.method == 2:
-            np.save(FILEPATH + 'models/mu2cond.npy',self.mu2)
+        np.save(FILEPATH + 'models/mu2cond.npy',self.mu2)
 
     def candidate(self,ks,n=DEFAULT_NUM_SAMPLES):
         """Returns the marginal variance of all location given that a location (ks) in the GMRF has been measured.
@@ -141,10 +88,7 @@ class spde:
             ks (integer): Array of indicies of possible candidate locations
             n (int, optional): Number of samples used in the Monte Carlo estimate. Defaults to 40.
         """
-        if self.method == 2:
-            z1 = np.random.normal(size = (self.n+2)*n).reshape((self.n+2),n) 
-        else:
-            z1 = np.random.normal(size = (self.n)*n).reshape((self.n),n) 
+        z1 = np.random.normal(size = (self.n+2)*n).reshape((self.n+2),n) 
         x = self.Q_fac.apply_Pt(self.Q_fac.solve_Lt(z1,use_LDLt_decomposition=False)) 
         res = np.zeros((self.n,ks.size))
         z2 = np.random.normal(size = n).reshape(1,n)*self.sigma[0]
@@ -155,8 +99,7 @@ class spde:
             U = V.transpose()/W
             c = S@x - z2
             x_ = x -  U.transpose()@c
-            if self.method == 2:
-                x_ = x_[:self.n,:] + x_[self.n,:] + self.mu3[:,np.newaxis]*x_[self.n+1,:][np.newaxis,:]
+            x_ = x_[:self.n,:] + x_[self.n,:] + self.mu3[:,np.newaxis]*x_[self.n+1,:][np.newaxis,:]
             res[:,i] = x_.var(axis=1)
         return(res)
 
@@ -168,21 +111,13 @@ class spde:
             ks ([k,]-array): k number of indicies describing the index of the measurment in the field. 
         """
         if ks.size>0:
-            if self.method == 2:
-                S = self.Stot[ks,:]
-                self.Q = self.Q + S.transpose()@S*1/self.sigma[0]**2
-                self.Q_fac.cholesky_inplace(self.Q)
-                self.mu2 = self.mu2 - self.Q_fac.solve_A(S.transpose().tocsc())@(S@self.mu2 - rel)*1/self.sigma[0]**2
-                self.mu = self.mu2[:self.n,0] + self.mu2[self.n,0] + self.mu3*self.mu2[self.n+1,0]
-                self.beta0 = self.mu2[self.n,0]
-                self.beta1 = self.mu2[self.n+1,0]
-            else:
-                mu = self.mu.reshape(-1,1)
-                S = self.Stot[ks,:]
-                self.Q[ks,ks] = self.Q[ks,ks] + 1/self.sigma[0]**2
-                self.Q_fac.cholesky_inplace(self.Q)
-                mu = mu - self.Q_fac.solve_A(S.transpose()@(S@mu-rel)*1/self.sigma[0]**2)
-                self.mu = mu.flatten()
+            S = self.Stot[ks,:]
+            self.Q = self.Q + S.transpose()@S*1/self.sigma[0]**2
+            self.Q_fac.cholesky_inplace(self.Q)
+            self.mu2 = self.mu2 - self.Q_fac.solve_A(S.transpose().tocsc())@(S@self.mu2 - rel)*1/self.sigma[0]**2
+            self.mu = self.mu2[:self.n,0] + self.mu2[self.n,0] + self.mu3*self.mu2[self.n+1,0]
+            self.beta0 = self.mu2[self.n,0]
+            self.beta1 = self.mu2[self.n+1,0]
 
     def mvar(self,Q_fac = None, n=DEFAULT_NUM_SAMPLES):
         """Monte Carlo Estimate of the marginal variance of a GMRF.
@@ -193,51 +128,30 @@ class spde:
         """
         if Q_fac is None:
             Q_fac = self.Q_fac
-        if self.method == 2:
-            z = np.random.normal(size = (self.n+2)*n).reshape((self.n+2),n)
-            data = Q_fac.apply_Pt(Q_fac.solve_Lt(z,use_LDLt_decomposition=False)) 
-            data = data[:self.n,:] + data[self.n,:] + self.mu3[:,np.newaxis]*data[self.n+1,:][np.newaxis,:]
-        else:
-            z = np.random.normal(size = self.n*n).reshape(self.n,n)
-            data = Q_fac.apply_Pt(Q_fac.solve_Lt(z,use_LDLt_decomposition=False)) 
+        z = np.random.normal(size = (self.n+2)*n).reshape((self.n+2),n)
+        data = Q_fac.apply_Pt(Q_fac.solve_Lt(z,use_LDLt_decomposition=False)) 
+        data = data[:self.n,:] + data[self.n,:] + self.mu3[:,np.newaxis]*data[self.n+1,:][np.newaxis,:]
         return(data.var(axis = 1))
 
     def resetQ(self):
         """Resets Q to initial values for the same grid
         """
-        self.M = 45
-        self.N = 45
-        self.P = 11
+        self.M = 50
+        self.N = 55
+        self.P = 6
         self.n = self.M*self.N*self.P
-        tmp = np.load(FILEPATH + 'models/SINMOD-NA.npy') # load fitted precision matrix Q
+        tmp = np.load(FILEPATH + 'models/NA.npy') # load fitted precision matrix Q
         self.Q = sparse.csc_matrix((np.array(tmp[:,2],dtype = "float32"), (tmp[:,0].astype('int32'), tmp[:,1].astype('int32'))), shape=(self.n,self.n))
         self.Q_fac = cholesky(self.Q)
-        if self.reduced:
-            self.reduce()
-        self.Stot = sparse.eye(self.n).tocsc()
-        if self.method == 2:
-            self.Q.resize((self.n + 2, self.n + 2))
-            self.Q = self.Q.tolil()
-            self.Q[self.n, self.n] = 0.02
-            self.Q[self.n + 1, self.n + 1] = 10
-            self.Q = self.Q.tocsc()
-            self.Q_fac = cholesky(self.Q)
-            
-            self.Stot.resize((self.n,self.n+2))
-            self.Stot = self.Stot.tolil()
-            self.Stot[:,self.n] = np.ones(self.n)
-            self.Stot[:,self.n+1] = self.mu3
-            self.Stot = self.Stot.tocsc()
-            self.mu = self.mu2[:self.n,0] + self.mu2[self.n,0] + self.mu3*self.mu2[self.n+1,0]
+        tmp = np.load(FILEPATH + 'models/S.npy')
+        self.Stot = sparse.csc_matrix((np.array(tmp[:,2],dtype = "float32"), (tmp[:,0].astype('int32'), tmp[:,1].astype('int32'))), shape=(self.n,self.n+2))
+        self.mu = self.mu2[:self.n,0] + self.mu2[self.n,0] + self.mu3*self.mu2[self.n+1,0]
         print("Q: ", self.Q.shape)
 
     def setThreshold(self):
         """Set threshold for Excursion set
         """
-        if self.reduced:
-            ind = np.load(FILEPATH + 'models/boundary_reduced.npy')
-        else:
-            ind = np.load(FILEPATH + 'models/boundary.npy')
+        ind = np.load(FILEPATH + 'models/boundary.npy')
         self.threshold = self.mu[ind].mean()
         print('Treshold is set to %.2f'%(self.threshold))
         np.save("models/threshold.npy", self.threshold)
@@ -249,12 +163,9 @@ class spde:
         print("Saved google coefficients.")
 
     def loadPrev(self):
-        if self.method == 2:
-            self.mu2 = np.load(FILEPATH + 'models/mu2cond.npy')
-            self.mu = self.mu2[:self.n,0] + self.mu2[self.n,0] + self.mu3*self.mu2[self.n+1,0]
-            print("Successfully loaded previous model!")
-        else:
-            print('Wrong method... Nothing is updated.')
+        self.mu2 = np.load(FILEPATH + 'models/mu2cond.npy')
+        self.mu = self.mu2[:self.n,0] + self.mu2[self.n,0] + self.mu3*self.mu2[self.n+1,0]
+        print("Successfully loaded previous model!")
 
     def postProcessing(self):
         self.setThreshold()
@@ -270,28 +181,17 @@ class spde:
         r = Q.row
         c = Q.col
         v = Q.data
-        if self.method == 1:
-            np.savez(FILEPATH + "models/currentModel.npz", model = self.model, method = self.method, reduce = self.reduced, Qr = r, Qc = c, Qv = v, mu = self.mu)
-        else:
-            np.savez(FILEPATH + "models/currentModel.npz", model = self.model, method = self.method, reduce = self.reduced, Qr = r, Qc = c, Qv = v, mu = self.mu, mu2 = self.mu2)
+        np.savez(FILEPATH + "models/currentModel.npz", Qr = r, Qc = c, Qv = v, mu = self.mu, mu2 = self.mu2)
         
     def loadModel(self):
         tmp = np.load(FILEPATH + 'models/currentModel.npz')
-        self.model = tmp['model']*1
-        self.method = tmp['method']*1
-        self.reduced = (tmp['reduce']*1).astype('bool')
-        self.P = 11
-        if self.reduced:
-            self.P = 7
         self.n = self.M*self.N*self.P
         self.mu = tmp['mu']*1
-        if self.method ==2:
-            self.mu2 = tmp['mu2']*1
-            self.beta0 = self.mu2[self.n]
-            self.beta1 = self.mu2[self.n+1]
-            self.Q = sparse.csc_matrix((np.array(tmp['Qv']*1,dtype = "float32"), ((tmp['Qr']*1).astype('int32'), (tmp['Qc']*1).astype('int32'))), shape=(self.n+2,self.n+2))
-        else:
-            self.Q = sparse.csc_matrix((np.array(tmp['Qv']*1,dtype = "float32"), ((tmp['Qr']*1).astype('int32'), (tmp['Qc']*1).astype('int32'))), shape=(self.n,self.n))
+        self.mu2 = tmp['mu2']*1
+        self.beta0 = self.mu2[self.n]
+        self.beta1 = self.mu2[self.n+1]
+        self.Q = sparse.csc_matrix((np.array(tmp['Qv']*1,dtype = "float32"), ((tmp['Qr']*1).astype('int32'), (tmp['Qc']*1).astype('int32'))), shape=(self.n+2,self.n+2))
+        
 
 
 if __name__ == "__main__":
