2f3bb533704646e09b937a68e593fc6b08bf47e9
Yaolin Ge <geyaolin@gmail.com>
2022-09-07 12:43:20 +0200

    update_HITL

--- Nidelva3D/src/packages.sh
+++ Nidelva3D/src/packages.sh
@@ -4,7 +4,8 @@ conda install -n TAICHI -c plotly plotly
 conda install -n TAICHI -c conda-forge scikit-sparse
 conda install -n TAICHI -c conda-forge netcdf4
 conda install -n TAICHI -c conda-forge scikit-gstat
-conda instal -n TAICHI -c conda-forge ros-rospy
 conda install -n TAICHI -c anaconda pandas
 conda install -n TAICHI -c conda-forge shapely
 conda install -n TAICHI -c conda-forge matplotlib
+conda install -n TAICHI -c anaconda yaml
+conda instal -n TAICHI -c conda-forge ros-rospy
